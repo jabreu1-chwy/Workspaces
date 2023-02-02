1cacd8d44cc93cf9516fa2947ab7cee62c86cc8f
Joel Abreu <87429916+jabreu1-chwy@users.noreply.github.com>
2023-02-02 14:26:29 -0500

    Update terminate_workspaces.py
    fixed the output

--- terminate_workspaces.py
+++ terminate_workspaces.py
@@ -26,39 +26,17 @@ for region in regions:
                 ]
             )
             if not len(response["FailedRequests"]) == 0:
-                termed_count += 1
-            else:
                 print(
-                    f"Failed to terminate WorkspaceId: {response['FailedRequests']}")
+                    f"Failed to terminate WorkspaceId: {response['FailedRequests']['WorkspaceId']} | {response['FailedRequests']['ErrorMessage']} {region}")
                 time.sleep(2)
+            else:
+                print(f"Successfully terminated WorkspaceId: {entry}")
+                termed_count += 1
         except ClientError as e:
             print(
-                f"Term Error for WorkspaceId: {entry} in region {region}: {e}")
+                f"Term Error for WorkspaceId: {entry} in region {region}: | Error: {e}")
         except Exception as e:
             print(f"Another error with {entry} in region {region}| Error: {e}")
 
 print(f"Sucessfully terminated {termed_count} Workspaces")
 
-
-# for region in regions:
-#     workspaces = boto3.client("workspaces", region_name=region)
-#     for entry in term_list:
-#         try:
-#             response = workspaces.terminate_workspaces(
-#                 TerminateWorkspaceRequests=[
-#                     {"WorkspaceId": str(entry)},
-#                 ]
-#             )
-#             if not len(response["FailedRequests"]) == 0:
-#                 termed_count += 1
-#             else:
-#                 print(
-#                     f"Failed to terminate WorkspaceId: {response['FailedRequests']}")
-#                 time.sleep(2)
-#         except ClientError as e:
-#             print(
-#                 f"Term Error for WorkspaceId: {entry} in region {region}: {e}")
-#         except Exception as e:
-#             print(f"Another error with {entry} in region {region}| Error: {e}")
-
-# print(f"Sucessfully terminated {termed_count} Workspaces")
